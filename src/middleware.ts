2533b8fdc8abc99f533a7cc10e26eac28b254fd3
Ricardo <ricardosv46@gmail.com>
2023-05-07 04:28:50 -0500

    fix rutas protegidas

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,37 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { getToken } from 'next-auth/jwt';
- 
+
 export async function middleware(req: NextRequest) {
   
   const session: any = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-  //validacion si no esta logeaddo
+ 
+
+   if (req.nextUrl.pathname.startsWith('/auth')) {
+    if (session) {
+      // Obtener el valor de "p" y decodificarlo
+        const queryParamP = new URLSearchParams(req.nextUrl.search).get('p');
+        const decodedP = queryParamP ? decodeURIComponent(queryParamP) : null;
+        const url = req.nextUrl.clone();
+        url.pathname = decodedP || '/';
+        url.search = '';
+        console.log({decodedP})
+        return NextResponse.redirect(url);
+    }
+  }
+ 
+ // validacion si no esta logeaddo
   if (!session) {
+    if (!req.nextUrl.pathname.startsWith('/auth')) {
     const requestedPage = req.nextUrl.pathname;
     const url = req.nextUrl.clone();
     url.pathname = `/auth/login`;
     url.search = `p=${requestedPage}`;
     return NextResponse.redirect(url);
   }
-
-
+}
+ 
+ 
   //validacion si esta logeado pero no es administrador
   const validRoles = ['admin','super-user','SEO'];
 
@@ -35,12 +52,10 @@ export async function middleware(req: NextRequest) {
     }
   }
  
- 
   return NextResponse.next();
 }
 
-
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/checkout/address', '/checkout/summary','/orders/history','/orders/:path*','/admin/:path*','/api/admin/:path*','/((?!api/admin/upload)api/admin/:path*)',]
+  matcher: ['/auth/:path*','/checkout/address', '/checkout/summary','/orders/history','/orders/:path*','/admin/:path*','/api/admin/:path*','/((?!api/admin/upload)api/admin/:path*)',]
 };
